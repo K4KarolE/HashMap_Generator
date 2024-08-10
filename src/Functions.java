65a4a7a66a74fa8229ed0d586935715d4b9347e0
k4karole <k4karole@gmail.com>
2024-08-10 06:26:56 +0100

    setters, action button added

--- src/Functions.java
+++ src/Functions.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+
 import java.awt.Desktop;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
@@ -16,30 +17,60 @@ import java.awt.datatransfer.StringSelection;
 public class Functions {
 
     static String[] mapType = {"Map", "HashMap", "LinkedHashMap", "TreeMap"};
-    static String mapTypeSelected = mapType[1];
+    static String mapTypeSelected = "";
+    public void setTypeSelected(String newValue) {mapTypeSelected = newValue;}
 
     static String[] resultAs = {"Map", "Function"};
-    static String resultAsSelected = resultAs[1];
+    static String resultAsSelected = "";
+    public void setResultAsSelected(String newValue) {resultAsSelected = newValue;}
 
     static String[] splitBy = {"Line break", ",", ";" ,"#"};
-    static String splitBySelected = splitBy[1];
+    static String splitBySelected = "";
+    public void setSplitBySelected(String newValue) {splitBySelected = newValue;}
 
     static String[] dataType = {"String", "Integer", "Double", "Float", "Long", "Byte", "Short", "Boolean", "Char"};
-    static String dataTypeKey = dataType[0];
-    static String dataTypeValue = dataType[0];
+    static String dataTypeKey = "";
+    public void setDataTypeKey(String newValue) {dataTypeKey = newValue;}
+    static String dataTypeValue = "";
+    public void setDataTypeValue(String newValue) {dataTypeValue = newValue;}
     
     static String[] resultActions = {"Display it in Notepad", "Copy to clipboard", "Both"};
-    static String resultActionsSelected = resultActions[2];
+    static String resultActionsSelected = "";
+    public void setResultActionsSelected(String newValue) {resultActionsSelected = newValue;}
 
-    static String keys = "<a>, <b>, <c>";
+
+    // KEYS
+    static String keys = "";
+    public void setKeys(String newValue) {keys = newValue;}
+
     static String keysRemoveLeft = "";
+    public void setKeysRemoveLeft(String newValue) {keysRemoveLeft = newValue;}
+
     static String keysRemoveRight = "";
+    public void setKeysRemoveRight(String newValue) {keysRemoveRight = newValue;}
+
+
+    // VALUES
     static String values = "#a-, #b-, #c-";
+    public void setValues(String newValue) {values = newValue;}
+
     static String valuesRemoveLeft = "";
+    public void setValuesRemoveLeft(String newValue) {valuesRemoveLeft = newValue;}
+
     static String valuesRemoveRight = "";
-    static String mapBody = "empty";
+    public void setValuesRemoveRight(String newValue) {valuesRemoveRight = newValue;}
+
+
     static String mapName = "myMap";
+    public void setMapName(String newValue) {mapName = newValue;}
+    
     static String functionName = "myFunction";
+    public void setFunctionName(String newValue) {functionName = newValue;}
+    
+    
+    static String mapBody = "empty";
+    public void setMapBodyBackToDefault() {mapBody = "empty";}
+
     static String mapResult = "";
 
     // PATH-FILE
@@ -49,6 +80,7 @@ public class Functions {
     // CLIPBOARD
     static Toolkit toolkit = Toolkit.getDefaultToolkit();
     static Clipboard clipboard = toolkit.getSystemClipboard();
+
 
     public String[] getRollDownOptions(String title) {
         HashMap<String, String[]> map = new HashMap<>();
@@ -60,6 +92,8 @@ public class Functions {
         return map.get(title);
     }
 
+
+    
     static String getTrimmedKey(String originalKey) {
         Integer beginIndex = keysRemoveLeft.length();
         Integer endIndex = originalKey.length() - keysRemoveRight.length();
@@ -75,17 +109,31 @@ public class Functions {
     
     
     static String addToMap(String key, String value) {
-        return mapName + ".put(\"" + key.strip() + "\", \"" + value.strip() + "\");"; 
+        if (dataTypeKey == "String") {
+            key = "\"" + key + "\"";
+        }
+        if (dataTypeKey == "Char") {
+            key = "'" + key + "'";
+        }
+
+
+        if (dataTypeValue == "String") {
+            value = "\"" + value + "\"";
+        }
+        if (dataTypeValue == "Char") {
+            value = "'" + value + "'";
+        }
+        return mapName + ".put(" + key + "," +  value + ");"; 
     }
     
     
-    static String[] getArrayOfKeys() {
+    public static String[] getArrayOfKeys() {
         if (splitBySelected == splitBy[0]) {splitBySelected = "\n";}
         return keys.split(splitBySelected);
     }
 
     
-    static String[] getArrayOfValues() {
+    public static String[] getArrayOfValues() {
         if (splitBySelected == splitBy[0]) {splitBySelected = "\n";}
         return values.split(splitBySelected);
     }
@@ -107,22 +155,30 @@ public class Functions {
     static String getBody() {
 
         Integer mapLength = getArrayOfKeys().length;
+        Integer valuesLength = getArrayOfValues().length;
+        String value;
 
-        for  (int i = 0; i < mapLength; i++) {
+   
+            for  (int i = 0; i < mapLength; i++) {
 
-            String key = getArrayOfKeys()[i];
-            key = getTrimmedKey(key);
-            String value = getArrayOfValues()[i];
-            value = getTrimmedValue(value);
+                String key = getArrayOfKeys()[i];
+                key = getTrimmedKey(key);
 
-            if (mapBody!="empty")  {
-                mapBody = mapBody + addToMap(key, value) + getNewLine(i, mapLength);
+                if (i < valuesLength) {    // Keys > Values
+                    value = getArrayOfValues()[i];
+                    value = getTrimmedValue(value);
+                }
+                else {value = "";}
+
+                if (mapBody!="empty")  {
+                    mapBody = mapBody + addToMap(key, value) + getNewLine(i, mapLength);
+                }
+                else {
+                    // First line of mapBody
+                    mapBody = addToMap(key, value) + getNewLine(i, mapLength); 
+                }
             }
-            else {
-                mapBody = addToMap(key, value) + getNewLine(i, mapLength);
-            }
-        }
-        return mapBody;
+            return mapBody;
     }
 
 
@@ -166,6 +222,7 @@ public class Functions {
             }
         catch (IOException e) {System.out.println("Could not open Notepad");}
     }
+    
     
     static void copyToClipboard() {
         StringSelection strSel = new StringSelection(mapResult);
--- src/Ui.java
+++ src/Ui.java
@@ -1,13 +1,17 @@
 package src;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import java.awt.Toolkit;
 import java.awt.Image;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Ui {
@@ -26,33 +30,88 @@ public class Ui {
         frame.getContentPane().setBackground(new java.awt.Color(243, 243, 243));
         frame.setResizable(false);
         frame.setVisible(true);
-    }
+    }    
 
+    static void warningMsgKeysValuesDiff() {
+        if (fs.getArrayOfKeys().length != fs.getArrayOfValues().length) {
+            JOptionPane.showMessageDialog(
+                frame,
+                "The amount of the Keys and Values are not matching!        \n\n"
+                        + "The result can be mismatched, truncated, \n"
+                        + "missing values can be replaced with empty string (\"\").",
+                "Warning",
+                JOptionPane.WARNING_MESSAGE);
+        }
+    }
 
     public static void main(String[] args) {
         
         Font combo_box_font_style = new Font("Times New Roman", Font.PLAIN, 14);
         Font field_font_style = new Font("Consolas", Font.BOLD, 18);
-        // Font label_font_style = new Font("Times New Roman", Font.PLAIN, 14);
-        // Font label_title_font_style = new Font("Times New Roman", Font.PLAIN, 18);
+        Font label_font_style = new Font("Times New Roman", Font.PLAIN, 14);
+        Font label_title_font_style = new Font("Times New Roman", Font.PLAIN, 18);
         
         JComboBox<String> comboBoxMapType = new JComboBox<>(fs.getRollDownOptions("mapType"));
 
         comboBoxMapType.setFont(combo_box_font_style);
 
-        JTextArea keysTextA = new JTextArea();
-        keysTextA.setFont(field_font_style);
+        JTextArea keysTextArea = new JTextArea();
+        keysTextArea.setFont(field_font_style);
         
-        keysTextA.setBounds(40, da.getPosY(1), da.WIDGET_WIDTH, da.WIDGET_HEIGHT);
         comboBoxMapType.setBounds(40, da.getPosY(-1), da.WIDGET_WIDTH, 25);
         
         
-        frame.add(keysTextA);
-        frame.add(comboBoxMapType);
+        JButton button = new JButton("Go");
+        button.setFont(label_font_style);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                fs.setTypeSelected(comboBoxMapType.getSelectedItem().toString());
+                fs.setResultAsSelected( "Function");
+                fs.setSplitBySelected( ",");
+                fs.setDataTypeKey( "String");
+                fs.setDataTypeValue( "Double");
+                fs.setResultActionsSelected( "Both");
+                
+                fs.setKeys(keysTextArea.getText());
+                fs.setKeysRemoveLeft( "");
+                fs.setKeysRemoveRight( "");
+                
+                // fs.setValues( """
+                //             a
+                //             b
+                //             c
+                //             """);
+                fs.setValues("a,b,c");
+                fs.setValuesRemoveLeft( "");
+                fs.setValuesRemoveRight( "");
+                
+                fs.setMapName( "--mapname");
+                fs.setFunctionName( "--funname");
+                
+                warningMsgKeysValuesDiff();
+                fs.goButtonAction();
+                fs.setMapBodyBackToDefault();
+            }
+        });
         
+        
+        
+        keysTextArea.setBounds(40, da.getPosY(1), da.WIDGET_WIDTH, da.WIDGET_HEIGHT);
+        button.setBounds(200, da.getPosY(-1), da.WIDGET_WIDTH, 25);
+        
+
+        frame.add(keysTextArea);
+        frame.add(comboBoxMapType);
+        frame.add(button);
+
         
         createWindow();
-        fs.goButtonAction();
+        
+
+        
         
     }
+
 }
